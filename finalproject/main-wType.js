4d76e3e4a53c15aeb80307f6bd64a3c649447807
evgcatipon <evgcatipon@users.noreply.github.com>
2020-12-13 10:54:11 -0500

    Update main-wType.js

--- finalproject/main-wType.js
+++ finalproject/main-wType.js
@@ -4,7 +4,7 @@ window.onload=function(){
 	submit.addEventListener('click', updateRecs, false);
 }
 
-//Object with clothing recommendations data
+//Object with recommendation card data
 var data = [
         {code: "fem0060", gender: "female", name: "Sports Bra", img: "img/female/sportsbra.png", desc: "Helps limit movement and provide support when running" },
         {code: "fem0060", gender: "female", name: "Singlet", img: "img/female/singlet.png", desc: "Keeps torso cool" },
@@ -46,25 +46,31 @@ var data = [
         {code: "sun0020", gender: "neutral", name: "Sunscreen", img: "img/sunscreen.jpg", desc: "Apply sunscreen 15min before a run and reapply every hour" }
 	];
 
+//jQuery function to check weather API based on zip code and display city, temperature and weather conditions
 jQuery(function() {
 	 $('#zipSend').click(function() {	
     var zip = $("#zipBox").val();
+	//check if zip code has a value	 
     if (zip !== '') {
       $.ajax({
 		url: "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&units=imperial&appid=136d4bb7c2e5b6e8ebf7dfd5dbac069f",
         dataType: "jsonp",
 		async: false,
         success: function(weatherData) {
+		  //variables to store city, weather and temperature json information from weather API	
           var city = weatherData.name;
 		  var apiCondition = weatherData.weather[0].description;
 		  var apiTemp = weatherData.main.temp.toFixed(1);
+		  //based on id element, display the API data	
           $("#city").text(city);
 		  $("#weatherCondition").text(apiTemp + '°F and ' + apiCondition);	
+		  //unhide divs
 		  document.getElementById("rightColumn").style.display = "inline";
 		  document.getElementById("currentConditions").style.display = "inline";	
         }
       });
     }else {
+		//if no zip code is entered, displays an alert 
 		alert('Enter a US zip code.');
 	}
   });
@@ -72,17 +78,14 @@ jQuery(function() {
 	
 
 
-//Display clothing recommendations
+//Function for creating clothing recommendations
 function updateRecs() {
-	
+	//unhide divs 
 	document.getElementById("rightColumn").style.display = "inline";
     document.getElementById("contentTitle").style.display = "inline";	
 	
 	//remove cards if they exist
 	$(".card").remove();
-	
-	//display clothing rec column
-    document.getElementById("contentTitle").style.display = "";
     
     //store form values
 	var gender = document.getElementById("gender");
@@ -114,6 +117,8 @@ function updateRecs() {
         div.append(p);
 			
 		};
+	
+	//alert displays if either gender or temperature hasn't been slected
     if ((gender.value === "") || (temp.value === "")){
 		alert('Gender and temperature are required.');
 		document.getElementById("rightColumn").style.display = "none";
@@ -121,18 +126,17 @@ function updateRecs() {
 	};
     //clothing recs displayed if female and 60 is selected
 	if ((gender.value === "1") && (temp.value === "60")) {
-		
+		//filter on this code value
 		var fem0060 = data.filter(function(clothing){
 			return clothing.code === 'fem0060';
 		});
-		//check fem0060 filtered array
-		console.log(fem0060);
+
 		//build cards with fem0060 filter
 		for (var i = 0; i < fem0060.length; i++){
 			buildClothingCard(fem0060[i]);
 		}
 		
-		//weather.value == "clear" 
+		//add cards based on weather condition 
 		if (weather.value == "sun") {
         //filter on sun0010 value
         var sun0010 = data.filter(function(clothing){
@@ -168,19 +172,18 @@ function updateRecs() {
         }
         
 	};
+	 //clothing recs displayed if female and 50 is selected
 if ((gender.value === "1") && (temp.value === "50")) {
-		
-		var fem0060 = data.filter(function(clothing){
+		//filter on this code value
+		var fem0050 = data.filter(function(clothing){
 			return clothing.code === 'fem0050';
 		});
-		//check fem0060 filtered array
-		console.log(fem0060);
-		//build cards with fem0060 filter
-		for (var i = 0; i < fem0060.length; i++){
-			buildClothingCard(fem0060[i]);
+		//build cards with fem0050 filter
+		for (var i = 0; i < fem0050.length; i++){
+			buildClothingCard(fem0050[i]);
 		}
 		
-		//weather.value == "clear" 
+		//add cards based on weather condition 
 		if (weather.value == "sun") {
         //filter on sun0010 value
         var sun0010 = data.filter(function(clothing){
@@ -215,19 +218,18 @@ if ((gender.value === "1") && (temp.value === "50")) {
 			}
         }
 	};
+	 //clothing recs displayed if female and 40 is selected
 	if ((gender.value === "1") && (temp.value === "40")) {
-		
-		var fem0060 = data.filter(function(clothing){
+		//filter on this code value
+		var fem0040 = data.filter(function(clothing){
 			return clothing.code === 'fem0040';
 		});
-		//check fem0060 filtered array
-		console.log(fem0060);
-		//build cards with fem0060 filter
-		for (var i = 0; i < fem0060.length; i++){
-			buildClothingCard(fem0060[i]);
+		//build cards with fem0040 filter
+		for (var i = 0; i < fem0040.length; i++){
+			buildClothingCard(fem0040[i]);
 		}
 		
-//weather.value == "clear" 
+		//add cards based on weather condition 
 		if (weather.value == "sun") {
         //filter on sun0020 value
         var sun0020 = data.filter(function(clothing){
@@ -263,19 +265,18 @@ if ((gender.value === "1") && (temp.value === "50")) {
         }
         
 	};
+	 //clothing recs displayed if female and 30 is selected
 	if ((gender.value === "1") && (temp.value === "30")) {
-		
-		var fem0060 = data.filter(function(clothing){
+		//filter on this code value
+		var fem0030 = data.filter(function(clothing){
 			return clothing.code === 'fem0030';
 		});
-		//check fem0060 filtered array
-		console.log(fem0060);
-		//build cards with fem0060 filter
-		for (var i = 0; i < fem0060.length; i++){
-			buildClothingCard(fem0060[i]);
+		//build cards with fem0030 filter
+		for (var i = 0; i < fem0030.length; i++){
+			buildClothingCard(fem0030[i]);
 		}
 		
-//weather.value == "clear" 
+		//add cards based on weather condition 
 		if (weather.value == "sun") {
         //filter on sun0020 value
         var sun0020 = data.filter(function(clothing){
@@ -311,19 +312,18 @@ if ((gender.value === "1") && (temp.value === "50")) {
         }
         
 	};
+	 //clothing recs displayed if male and 60 is selected
 	if ((gender.value === "2") && (temp.value === "60")) {
-		
-		var fem0060 = data.filter(function(clothing){
+		//filter on this code value
+		var mal0060 = data.filter(function(clothing){
 			return clothing.code === 'mal0060';
 		});
-		//check fem0060 filtered array
-		console.log(fem0060);
-		//build cards with fem0060 filter
-		for (var i = 0; i < fem0060.length; i++){
-			buildClothingCard(fem0060[i]);
+		//build cards with mal0060 filter
+		for (var i = 0; i < mal0060.length; i++){
+			buildClothingCard(mal0060[i]);
 		}
 		
-		//weather.value == "clear" 
+		//add cards based on weather condition 
 		if (weather.value == "sun") {
         //filter on sun0010 value
         var sun0010 = data.filter(function(clothing){
@@ -359,19 +359,19 @@ if ((gender.value === "1") && (temp.value === "50")) {
         }
         
 	};
+	 //clothing recs displayed if male and 50 is selected
 	if ((gender.value === "2") && (temp.value === "50")) {
-		
-		var fem0060 = data.filter(function(clothing){
+		//filter on this code value
+		var mal0050 = data.filter(function(clothing){
 			return clothing.code === 'mal0050';
 		});
-		//check fem0060 filtered array
-		console.log(fem0060);
-		//build cards with fem0060 filter
-		for (var i = 0; i < fem0060.length; i++){
-			buildClothingCard(fem0060[i]);
+
+		//build cards with mal0050 filter
+		for (var i = 0; i < mal0050.length; i++){
+			buildClothingCard(mal0050[i]);
 		}
 		
-		//weather.value == "clear" 
+		//add cards based on weather condition 
 		if (weather.value == "sun") {
         //filter on sun0010 value
         var sun0010 = data.filter(function(clothing){
@@ -407,19 +407,18 @@ if ((gender.value === "1") && (temp.value === "50")) {
         }
         
 	};
+	 //clothing recs displayed if male and 40 is selected
 	if ((gender.value === "2") && (temp.value === "40")) {
-		
-		var fem0060 = data.filter(function(clothing){
+		//filter on this code value
+		var mal0040 = data.filter(function(clothing){
 			return clothing.code === 'mal0040';
 		});
-		//check fem0060 filtered array
-		console.log(fem0060);
 		//build cards with fem0060 filter
-		for (var i = 0; i < fem0060.length; i++){
-			buildClothingCard(fem0060[i]);
+		for (var i = 0; i < mal0040.length; i++){
+			buildClothingCard(mal0040[i]);
 		}
 		
-//weather.value == "clear" 
+		//add cards based on weather condition 
 		if (weather.value == "sun") {
         //filter on sun0020 value
         var sun0020 = data.filter(function(clothing){
@@ -455,19 +454,19 @@ if ((gender.value === "1") && (temp.value === "50")) {
         }
         
 	};
+	//clothing recs displayed if male and 30 is selected
 	if ((gender.value === "2") && (temp.value === "30")) {
-		
-		var fem0060 = data.filter(function(clothing){
+		//filter on this code value
+		var mal0030 = data.filter(function(clothing){
 			return clothing.code === 'mal0030';
 		});
-		//check fem0060 filtered array
-		console.log(fem0060);
-		//build cards with fem0060 filter
-		for (var i = 0; i < fem0060.length; i++){
-			buildClothingCard(fem0060[i]);
+
+		//build cards with mal0030 filter
+		for (var i = 0; i < mal0030.length; i++){
+			buildClothingCard(mal0030[i]);
 		}
 		
-//weather.value == "clear" 
+		//add cards based on weather condition 
 		if (weather.value == "sun") {
         //filter on sun0020 value
         var sun0020 = data.filter(function(clothing){
